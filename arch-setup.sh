b580b25576be48721aa473d5730c281b1e35c148
Clevanilson da Silva Oliveira <Clevanilson.silvaoliveira@gmail.com>
2026-05-05 23:17:39 -0300

    Create arch-setup.sh

---
+++ arch-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+echo "Updating system"
+
+sudo pacman -Syyuu --noconfirm
+
+echo "Installing steam"
+
+sudo pacman -Syu steam --noconfirm
+
+echo "Installing yay"
+
+sudo pacman -S --needed base-devel git
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd
+
+echo "installing brave"
+
+yay -S brave-bin --noconfirm
+
+echo "Creating develop dir"
+
+mkdir develop
